223987268aa98532d0077194ffa9d493004872bb
Victor Mamani <victoryari61@gmail.com>
2026-05-26 07:44:35 -0500

    Fix Vercel: BD TiDB + CA cert + SSL config + build assets

--- config/database.php
+++ config/database.php
@@ -61,7 +61,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => filter_var(env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', true), FILTER_VALIDATE_BOOLEAN),
+            ], fn($v) => $v !== null && $v !== '') : [],
         ],
 
         'mariadb' => [
